efaf0ca50f92566ac0b98a506d2aa78deff58408
Scott Swezey <github@abadox.biz>
2013-11-26 14:04:08 -0800

    fixed typo in installer

--- wp-comment-killer/manual-force-install.php
+++ wp-comment-killer/manual-force-install.php
@@ -2,8 +2,8 @@
   // Execute via the web or CLI to forcibly install this plugin. This installer tested on 3.7, and more likely to break as time goes on.
 
   // Setup enviroment for plugin installation
-  require( dirname(__FILE__) . '../../../wp-load.php' );
-  require( dirname(__FILE__) . '../../../wp-admin/includes/plugin.php' );
+  require( dirname(__FILE__) . '/../../../wp-load.php' );
+  require( dirname(__FILE__) . '/../../../wp-admin/includes/plugin.php' );
 
   // Actually activate the plugin
   activate_plugin('wp-comment-killer/wp-comment-killer.php', '', true);
