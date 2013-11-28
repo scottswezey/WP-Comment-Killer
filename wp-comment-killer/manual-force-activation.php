9b05b78d6dd2c87f39c6e6e82926d6e6db27ced6
Scott Swezey <github@abadox.biz>
2013-11-28 12:18:48 -0800

    Added license to installer. network installs now require code modification to function.

--- wp-comment-killer/manual-force-activation.php
+++ wp-comment-killer/manual-force-activation.php
@@ -1,5 +1,27 @@
 <?PHP
   // Execute via the web or CLI to forcibly install this plugin. This installer tested on 3.7, and more likely to break as time goes on.
+  
+  /*  The MIT License (MIT)
+	    Copyright (c) 2013 Scott Swezey
+
+  	  Permission is hereby granted, free of charge, to any person obtaining a copy
+  	  of this software and associated documentation files (the "Software"), to deal
+  	  in the Software without restriction, including without limitation the rights
+  	  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  	  copies of the Software, and to permit persons to whom the Software is
+  	  furnished to do so, subject to the following conditions:
+
+  	  The above copyright notice and this permission notice shall be included in all
+  	  copies or substantial portions of the Software.
+
+  	  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  	  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  	  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  	  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  	  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  	  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  	  SOFTWARE.
+  */
 
   // Setup enviroment for plugin installation
   require( dirname(__FILE__) . '/../../../wp-load.php' );
@@ -7,9 +29,10 @@
 
   // Actually activate the plugin
   if ( is_multisite() ) {
-    activate_plugin('wp-comment-killer/wp-comment-killer.php', '', true); //multi-sites
+    wp_die('This appears to be a multisite/network install. Remove or comment out line '.__LINE__.' if you wish to proceed.');
+    activate_plugin('wp-comment-killer/wp-comment-killer.php', '', true);
   } else {
-    activate_plugin('wp-comment-killer/wp-comment-killer.php', '', false); //single sites
+    activate_plugin('wp-comment-killer/wp-comment-killer.php', '', false);
   }
 
   //Show that it worked
