b0d288135941cce9560109888796bb9f66573f0f
Scott Swezey <github@abadox.biz>
2013-11-28 15:23:45 -0800

    default archive no longer contains installer script

--- build_zip.sh
+++ build_zip.sh
@@ -1,5 +1,10 @@
-echo "Removing existing wp-comment-killer.zip..."
-rm wp-comment-killer.zip
+echo "Removing existing archives..."
+rm wp-comment-killer.zip wp-comment-killer-w-installer.zip
+
 echo ""
-echo "Generating new zip file..."
-zip -r wp-comment-killer.zip wp-comment-killer/
+echo "Generating new zip file w/installer..."
+zip -r wp-comment-killer-w-installer.zip wp-comment-killer/
+
+echo ""
+echo "Generating new zip file w/out installer..."
+zip -r wp-comment-killer.zip wp-comment-killer/ -x manual*
