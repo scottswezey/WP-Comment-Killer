dfcb3a74895ae7afd8100de5ec086a660b68cd7a
Scott Swezey <github@abadox.biz>
2013-12-05 07:42:16 -0800

    Removed installer archive from build script.

--- build_zip.sh
+++ build_zip.sh
@@ -1,9 +1,9 @@
 echo "Removing existing archives..."
 rm wp-comment-killer.zip wp-comment-killer-w-installer.zip
 
-echo ""
-echo "Generating new zip file w/installer..."
-zip -r wp-comment-killer-w-installer.zip wp-comment-killer/
+# echo ""
+# echo "Generating new zip file w/installer..."
+# zip -r wp-comment-killer-w-installer.zip wp-comment-killer/
 
 echo ""
 echo "Generating new zip file w/out installer..."
